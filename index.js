2dc9cfe5a05aa95060f5e794819cb71ad75099c8
William Kay Kephart <wllmkphrt@gmail.com>
2022-10-11 17:05:04 -0400

    commit tree faef00fa4b061a430345f7ee7084757924550f65

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(driverList){
+    const firstTwo = [driverList[0], driverList[1]];
+    return firstTwo;
+}
+const returnLastTwoDrivers = function(driverList){
+    const lastTwo = [driverList[driverList.length - 2], driverList[driverList.length - 1]];
+    return lastTwo;
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare){return fare * multiplier};
+}
+function fareDoubler(fare){
+    const newFare = createFareMultiplier(2)(fare);
+    return newFare;
+}
+function fareTripler(fare){
+    const newFare = createFareMultiplier(3)(fare);
+    return newFare;
+}
+function selectDifferentDrivers(driverList, someFunction){
+    return someFunction(driverList);
+}
